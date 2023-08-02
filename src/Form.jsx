99479d1a616873760eccb42c5a5704ed8ae6d602
Shanice Griffin <shanicegriffin@Shanices-MacBook-Air.local>
2023-08-01 22:11:05 -0400

    added functions

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,12 +1,47 @@
 import React from "react";
 import "./Form.css";
+import { useState } from "react";
 
 function Form() {
+  const [newInput, setNewInput] = useState([])
+  const [selectOption, setSelectOption] = useState("")
+
+  const handleTextchange = (e) => {
+    setNewInput([e.target.value])
+  }
+
+  const handleSelectChange = (e) => {
+    setSelectOption(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setNewInput([])
+    setSelectOption("")
+  }
+
+  const sum = (numbers) => {
+    return numbers.reduce((a,b)=> a + b, 0);
+  }
+
+  const average = (numbers) => {
+    return sum(numbers)/numbers.length;
+  }
+
+  const mode = (numbers) => {
+    let counts = numbers.reduce(
+      (acc, value) => ({ ...acc, [value]: (acc[value] || 0) + 1 }),
+      {});
+      let maxCount = Math.max(...Object.values(counts));
+      let mode = Object.keys(counts).find(key => counts[key] === maxCount);
+      return mode;
+  }
+
   return (
     <>
-      <form>
-        <input id="values" name="values" type="text" />
-        <select id="operation" name="operation">
+      <form onSubmit={handleSubmit}>
+        <input id="values" name="values" type="text" value={newInput} onChange={handleTextchange} htmlfor="operations" />
+        <select id="operation" name="operation" value={selectOption} onChange={handleSelectChange}>
           <option value=""></option>
           <option value="sum">sum</option>
           <option value="average">average</option>
