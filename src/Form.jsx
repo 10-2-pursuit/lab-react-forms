c6e20f932213651a65f0ae3cf0f6b98238a4d68f
Tonesha Rose <131703989+tonesharose31@users.noreply.github.com>
2023-08-05 03:08:44 -0400

    created a calculated reults function which takes in arguments for user input

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,35 +1,77 @@
 import React from "react";
-import {useState} from "react"
+import { useState } from "react";
 import "./Form.css";
 
 function Form() {
   const [inputValues, setInputValues] = useState("");
-  const[ SelectedOperation,setSelectedOperation]= useState("");
-  
+  const [selectedOperation, setSelectedOperation] = useState("");
+  const [result, setResult] = useState("");
+
   const handleInputchange = (e) => {
-  setInputValues(e.target.value);
-};
+    setInputValues(e.target.value);
+  };
 
-const handleOperationChange = (e) => {
-  setSelectedOperation(e.target.value);
-};
+  const handleOperationChange = (e) => {
+    setSelectedOperation(e.target.value);
+  };
 
-const handleFormSubmit = (e) => {
-  e.preventDefault();
-};
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
 
-const numbers = inputValues.split(",").map((num)=> parseFloat(num.trim());)
+    const numbers = inputValues.split(",").map((num) => parseFloat(num.trim()));
 
-if(numbers.some(isNaN)){
-  SetResult(`Invalid input.`);
-  return;
-}
+    if (numbers.some(isNaN)) {
+      setResult(`Invalid input.`);
+      return;
+    }
+    let calculatedResults;
+    switch (selectedOperation) {
+      case "sum":
+        calculatedResults = numbers.reduce((acc, curr) => acc + curr, 0);
+        break;
+      case "average":
+        calculatedResults =
+          numbers.reduce((acc, curr) => acc + curr, 0) / numbers.length;
+        break;
+      case "mode":
+        const frequencyMap = {};
+        numbers.forEach((num) => {
+          frequencyMap[num] = (frequencyMap[num] || 0) + 1;
+        });
+        let maxFrequency = 0;
+        let mode = null;
+        for (const num in frequencyMap) {
+          if (frequencyMap[num] > maxFrequency) {
+            maxFrequency = frequencyMap[num];
+            mode = num;
+          }
+        }
+        calculatedResults = mode;
+        break;
+
+      default:
+        calculatedResults = "";
+        break;
+    }
+    setResult(calculatedResults);
+  };
 
   return (
     <>
       <form onSubmit={handleFormSubmit}>
-        <input id="values" name="values" type="text" value={inputValues} onChange={handleInputchange}/>
-        <select id="operation" name="operation" value={SelectedOperation} onChange={handleOperationChange}>
+        <input
+          id="values"
+          name="values"
+          type="text"
+          value={inputValues}
+          onChange={handleInputchange}
+        />
+        <select
+          id="operation"
+          name="operation"
+          value={selectedOperation}
+          onChange={handleOperationChange}
+        >
           <option value=""></option>
           <option value="sum">sum</option>
           <option value="average">average</option>
@@ -38,7 +80,7 @@ if(numbers.some(isNaN)){
         <button type="submit">Calculate</button>
       </form>
       <section id="result">
-        <p>{/* display calculated results here*/}</p>
+        <p>{result}</p>
       </section>
     </>
   );
