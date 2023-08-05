164a18a19e7a0f205717289c807b55bda244d71d
Tonesha Rose <131703989+tonesharose31@users.noreply.github.com>
2023-08-05 00:48:43 -0400

    created an input value state to handle the event of user input

--- src/Form.jsx
+++ src/Form.jsx
@@ -2,10 +2,14 @@ import React from "react";
 import "./Form.css";
 
 function Form() {
+  const [inputValues, setInputValues] = useState("")
+
+  const handleInputchange = (e) => 
+  setInputValues(e.target.value);
   return (
     <>
       <form>
-        <input id="values" name="values" type="text" />
+        <input id="values" name="values" type="text" value={inputValues} onChange={handleInputchange}/>
         <select id="operation" name="operation">
           <option value=""></option>
           <option value="sum">sum</option>
