b760dc38b5b22d92a0f5480bd7c796fbc19ccb07
Tonesha Rose <131703989+tonesharose31@users.noreply.github.com>
2023-08-05 01:26:07 -0400

    created a onchange state for selected operation

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,17 +1,23 @@
 import React from "react";
-import React, {useState} from "react"
+import {useState} from "react"
 import "./Form.css";
 
 function Form() {
-  const [inputValues, setInputValues] = useState("")
-
-  const handleInputchange = (e) => 
+  const [inputValues, setInputValues] = useState("");
+  const[ SelectedOperation,setSelectedOperation]= useState("");
+  
+  const handleInputchange = (e) => {
   setInputValues(e.target.value);
+};
+
+const handleOperationChange = (e) => {
+  setSelectedOperation(e.target.value);
+};
   return (
     <>
       <form>
         <input id="values" name="values" type="text" value={inputValues} onChange={handleInputchange}/>
-        <select id="operation" name="operation">
+        <select id="operation" name="operation" value={SelectedOperation} onChange={handleOperationChange}>
           <option value=""></option>
           <option value="sum">sum</option>
           <option value="average">average</option>
