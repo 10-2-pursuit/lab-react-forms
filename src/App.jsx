1b05adb43da70c4dd58653a6d36dc1b58b8c0e5b
addisjackson <addisjackson@pursuit.org>
2023-08-19 18:52:47 -0400

    finishes lab

--- src/App.jsx
+++ src/App.jsx
@@ -2,79 +2,15 @@ import React from "react";
 import Form from "./Form";
 import "./App.css";
 
-    
 function App() {
-const [selectedOption, setSelectedOption] = useState("sum");
-const [values, setValues] = useState("");
-const [array, setArray] = useState("")
-const [result, setResult] = useState("")
-const [error, setError] = useState(false)
-
-const sumArray = (numbers) => {
-  return numbers.reduce((acc, curr) => acc + curr, 0);
-}
-
-const averageArray = (numbers) => {
-  return sumArray(numbers) / numbers.length;
-}
-
-function modeArray (numbers) {
-  const counts = numbers.reduce((acc, curr) => {
-    acc[curr] = (acc[curr] || 0) + 1;
-    return acc;
-  }, {});
-  const max = Math.max(...Object.values(counts));
-  return Object.keys(counts).find(key => counts[key] === max);
-}
-
-function sertError(error) {
-  setError(error);
-}
-
-const handleValuesChange = (event) => {
-  setValues(event.target.value);
-  sertError(false);
-}
-
-const handleSelectedOptionChange = (event) => {
-  setSelectedOption(event.target.value);
-}
-
-const calculatedResult = () => {
-  const numbers = array.split(",").map((Number) => parseInt(Number.trim(), 10))
-
-  if(numbers.some((Number)=> isNaN(Number))) {
-    setError(true)
-        return;
-  }
-
-  setError(false);
-
-  let calculatedResult;
-
-if (selectedOption.value = "sum" ) {
-  calculatedResult.value = sumArray(numbers);
-} else if (selectedOption.value = "average") {
-  calculatedResult.value = averageArray(numbers);
-} else if (selectedOption.value = "mode") {
-  calculatedResult.value = modeArray(numbers);
-} else {
-  calculatedResult.value = Error;
-}
-
-setResult(calculatedResult);
-setValues("");
-}
-
   return (
     <div className="App">
-    
-    <main>
-      <p>Enter each number in the array, separated by a ','</p>
-      <Form />
-    </main>
-</div>
-  )
+      <main>
+        <p>Enter each number in the array, separated by a ','</p>
+        <Form />
+      </main>
+    </div>
+  );
 }
 
 export default App;
--- src/Form.jsx
+++ src/Form.jsx
@@ -1,93 +1,104 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Form.css";
 
-function Form({
-  selectedOption,
-  array,
-  error,
-  handleArrayChange,
-  handleSelectedOptionChange,
-  calculateResult,
-}) {
-  const sumArray = (numbers) => {
-    return numbers.reduce((acc, curr) => acc + curr, 0);
-  };
-
-  const averageArray = (numbers) => {
-    return sumArray(numbers) / numbers.length;
-  };
-
-  function modeArray(numbers) {
-    const counts = numbers.reduce((acc, curr) => {
-      acc[curr] = (acc[curr] || 0) + 1;
-      return acc;
-    }, {});
-    const max = Math.max(...Object.values(counts));
-    return Object.keys(counts).find((key) => counts[key] === max);
-  }
+function Forms() {
+  const [numbers, setNumbers] = useState("");
+  const [selectedOption, setSelectedOption] = useState("");
+  const [result, setResult] = useState("");
+  const [hasError, setHasError] = useState(false);
 
   const handleValuesChange = (event) => {
-    handleArrayChange(event);
+    const inputChar = event.target.value.slice(-1);  
+    if (inputChar === "," || !isNaN(inputChar)) {
+      setNumbers(event.target.value);
+      setHasError(false); 
+    } else {
+      setHasError(true); 
+    }
+  };
+  
+
+  const handleSelectedOptionChange = (event) => {
+    setSelectedOption(event.target.value);
   };
 
-  const calculatedResult = () => {
-    const numbers = array.split(",").map((Number) => parseInt(Number.trim(), 10));
+  const handleCalculateResult = () => {
+    const numberArray = numbers.split(",").map(Number);
 
-    if (numbers.some((Number) => isNaN(Number))) {
-      // Set error if there are NaN values
-      calculateResult(true);
-      return;
-    }
-
-    // Reset error state if input is valid
-    calculateResult(false);
-
-    let resultValue;
-
-    if (selectedOption === "sum") {
-      resultValue = sumArray(numbers);
-    } else if (selectedOption === "average") {
-      resultValue = averageArray(numbers);
-    } else if (selectedOption === "mode") {
-      resultValue = modeArray(numbers);
+    if (numberArray.some(isNaN)) {
+      setHasError(true);
+      setResult("Invalid input.");
     } else {
-      resultValue = "Error";
-    }
+      setHasError(false);
 
-    calculateResult(resultValue);
+      let resultValue;
+
+      switch (selectedOption) {
+        case "sum":
+          resultValue = numberArray.reduce((acc, curr) => acc + curr, 0);
+          break;
+        case "average":
+          resultValue =
+            numberArray.reduce((acc, curr) => acc + curr, 0) / numberArray.length;
+          break;
+        case "mode":
+          const counts = {};
+          let maxCount = 0;
+          let mode = [];
+
+          for (const num of numberArray) {
+            counts[num] = (counts[num] || 0) + 1;
+            if (counts[num] > maxCount) {
+              maxCount = counts[num];
+              mode = [num];
+            } else if (counts[num] === maxCount) {
+              mode.push(num);
+            }
+          }
+
+          if (mode.length === numberArray.length) {
+            resultValue = "No mode.";
+          } else {
+            resultValue = mode.join(", ");
+          }
+          break;
+        default:
+          resultValue = "";
+      }
+
+      setResult(`Result: ${resultValue}`);
+      setNumbers(""); 
+      setSelectedOption(""); 
+    }
   };
 
   return (
-    <>
-      <form>
-        <input
-          id="values"
-          name="values"
-          type="text"
-          value={array}
-          onChange={handleValuesChange}
-          className={error ? "error" : ""}
-        />
-        <select
-          id="operation"
-          name="operation"
-          value={selectedOption}
-          onChange={handleSelectedOptionChange}
-          className={error ? "error" : ""}
-        >
-          <option value=""></option>
-          <option value="sum">sum</option>
-          <option value="average">average</option>
-          <option value="mode">mode</option>
-        </select>
-        <button type="button" onClick={calculatedResult}>
-          Calculate
-        </button>
-      </form>
+    <div className="App">
+      <h1>Number Calculator</h1>
+      <input
+        placeholder="Enter numbers"
+        type="text"
+        value={numbers}
+        onChange={handleValuesChange}
+        className={hasError ? "error" : ""}
+      />
+      <select
+        placeholder="Select operation"
+        value={selectedOption}
+        onChange={handleSelectedOptionChange}
+        className={hasError ? "error" : ""}
+      >
+        <option value=""></option>
+        <option value="sum">sum</option>
+        <option value="average">average</option>
+        <option value="mode">mode</option>
+      </select>
+      <button onClick={handleCalculateResult}>Calculate</button>
       <section id="result">
-        <p>{error ? "Error: Invalid input" : result !== "" ? `Result: ${result}` : ""}</p>
+        <p>{hasError ? "Error: Invalid input" : result}</p>
       </section>
-    </>
+    </div>
   );
 }
 
-export default Form;
+export default Forms;
